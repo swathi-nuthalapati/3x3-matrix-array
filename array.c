d15672085a92ea63236c5d16d17be64f879fa0c5
swathi <chittinuthalapati@gmail.com>
2020-09-30 11:06:56 -0700

    created array.c
    this file prints 3x3 matrix array

---
+++ array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int a[3][3];
+int i,j;
+printf("Input elements of the matrix:");
+    for(i=0;i<3;i++)
+    {
+       for(j=0;j<3;j++)
+       {
+             printf("\n elements [%d],[%d]:",i,j);
+             scanf("%d",&s[i][j]);
+        }
+     }
+  printf("\n the matrix is:\n");
+  for(i=0;i<3;i++)
+  {
+  printf("\n");
+         for(j=0;j<3;j++)
+         {
+         printf("%d\t",a[i][j]);
+         }
+   }
+   printf("\n\n");
+   getch();
+   }
